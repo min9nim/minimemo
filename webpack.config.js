0bc0a69e5da0a5920676364421c66fd95f7781d9
songmingu <min9nim@gmail.com>
2018-01-08 18:31:48 +0900

    webpack.config.js 파일 위치 변경
    minimemo
    => minimemo/public

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('/usr/local/lib/node_modules/webpack');
+
+module.exports = {
+    entry: {
+        bundle: './app.js',
+    }
+
+    , output: {
+        path: path.resolve(__dirname, '.'),
+        filename: '[name].js'
+    }
+
+    , module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                include: path.join(__dirname, '.'),
+                loaders: 'babel-loader', //babel에서 수정 (더이상 -loader를 생략할 수 없음)
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }
+
+
+    , plugins: [
+        new webpack.optimize.UglifyJsPlugin({     // es6이상은 지원하지 못함
+            sourceMap: true,
+            compress: {
+                warnings: true,
+            },
+        })
+    ]
+
+};
